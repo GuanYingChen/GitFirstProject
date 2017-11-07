3438954779cb69966eedb413a8afd0069520b3c3
Amber <splendid1011@gmail.com>
2017-11-07 13:52:50 +0800

    modify register.py by Amber

--- register.py
+++ register.py
@@ -6,6 +6,8 @@ import ConfigParser
 import click
 from utils import common_util
 from controller.register_cmd import RegisterCommand
+import pandas
+import numpy
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -16,8 +18,8 @@ default_api_port = common_util.get_conf('HippoManagerAPI', 'port')
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-p', '--project_home', required=True)
-@click.option('-s', '--service_name', help='Service name, Default: last directory name from ${project_home}')
+@click.option('-p', '--project', required=True)
+@click.option('-s', '--service', help='Service name, Default: last directory name from ${project_home}')
 @click.option('-c', '--run_cmd', help='command for run service, you can use \"{PROJECT_HOME}\" variable to build command (e.g. "sh {PROJECT_HOME}/bin/message_client.py")')
 @click.option('--client_ip', default=common_util.get_ip(), help='Client server IP, Default: {}'.format(common_util.get_ip()))
 @click.option('--api_host', default=default_api_host, help='hippo manager api host, Default: {}'.format(default_api_host))
@@ -28,8 +30,8 @@ def register(client_ip, project_home, service_name, run_cmd, api_host, api_port,
     cmd = RegisterCommand(api_url)
     cmd.execute(client_ip=client_ip, project_home=project_home,
                 service_name=service_name, run_cmd=run_cmd, user=user)
+    print "Done!"
 
 
 if __name__ == '__main__':
     register()
-
